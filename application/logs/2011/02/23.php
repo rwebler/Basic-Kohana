896c6ba358d783adf028171f8eae06859777b23f
Rodrigo Webler <rwebler@gmail.com>
2011-02-23 10:46:02 -0300

    adding Kohana and Zend with public_html

---
+++ application/logs/2011/02/23.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-02-23 06:34:14 --- ERROR: Http_Exception_404 [ 404 ]: Unable to find a route to match the URI: images/4x4.gif ~ SYSPATH/classes/kohana/request.php [ 733 ]
